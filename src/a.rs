8b5ccf4d20d3fc72e556411be7727651700cdc8e
Fuzail <mohdfuzailkazi14@gmail.com>
2025-02-17 14:53:07 +0530

    init

---
+++ src/a.rs
@@ -0,0 +1,33 @@
+use rand::Rng;
+use std::io;
+
+fn main() {
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    
+    println!("Guess the number between 1 and 100!");
+
+    loop {
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read input");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid number!");
+                continue;
+            }
+        };
+
+        if guess < secret_number {
+            println!("Too low!");
+        } else if guess > secret_number {
+            println!("Too high!");
+        } else {
+            println!("You got it! The secret number was {}", secret_number);
+            break;
+        }
+    }
+}
