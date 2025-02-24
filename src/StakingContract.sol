eff942d238f1661ea54453e9f826c25a36aae4a0
Fuzail <mohdfuzailkazi14@gmail.com>
2025-02-24 17:06:43 +0530

    staking contract skeleton

---
+++ src/StakingContract.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.13;
+
+
+import "forge-std/Test.sol";
+
+import "src/OrcaCoin.sol";
+
+contract StakingContract{
+
+    constructor(){}
+
+    function stake() {
+        
+    }
+
+    function unstake() {
+        
+    }
+
+    function getRewards() {
+        
+    }
+
+    function claimRewards(){}
+
+     function balanceOf(){}
+}
